58acee0cfaeb2a3137c181a2881a8dd266d587d6
ssankaranarayanan@my365.bellevue.edu <61958788+rengsankar1986@users.noreply.github.com>
2020-12-13 23:17:37 -0600

    DSC530 -Week2
    DSC530

---
+++ DSC530_Week2.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 12/13/2020
+@author: Sri Renganathan Sankaranarayanan
+Class: DSC530
+Assignment: 2.1 Preparing for EDA
+"""
+
+# Display the text “Hello World! I wonder why that is always the default coding text to start with”
+print("Hello World! "
+      "I wonder why that is always the default coding text to start with.")
+
+# Add two numbers together
+print("2 + 3 is", 2 + 3)
+
+# Subtract a number from another number
+print("5 - 1 is", 5 - 1)
+
+# Multiply two numbers
+print("9 x 6 is", 9 * 6)
+
+# Divide between two numbers
+print("8", chr(247),"2 is", int(8 / 2))
+
+# Concatenate two strings together (any words)
+a = " Texas is great "
+b = "for family"
+c = a + b
+print(c)
+
+# Create a list of 4 items (can be strings, numbers, both)
+listFourItems = ["apple", "orange", "pear", 4]
+print("Here's a list of 4 items:", listFourItems)
+
+# Append an item to your list (again, can be a string, number)
+listFourItems.append("item 5")
+print("Updated list:", listFourItems)
+
+# Create a tuple with 4 items (can be strings, numbers, both)
+tupleFourItems = ("james", "vasanth", "advaith", "vedanth")
+print("this is a tuple:", tupleFourItems)
